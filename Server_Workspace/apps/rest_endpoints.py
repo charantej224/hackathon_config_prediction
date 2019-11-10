d001cb6e34f3e3ce740868a11bd75f65ebf664d4
charantej224 <charantej.career@gmail.com>
2019-11-10 17:57:16 -0600

    cleaned code

--- Server_Workspace/apps/rest_endpoints.py
+++ Server_Workspace/apps/rest_endpoints.py
@@ -10,21 +10,6 @@ app = Flask(__name__)
 CORS(app)
 dataLoader = DataLoader()
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
 
 @app.route('/config-prediction/api/v1.0/service/all-data', methods=['GET'])
 def get_all_data():
